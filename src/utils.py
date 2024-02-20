947c11a843bc31c7aa343918a265a5548cfde619
Niko <niko.konzack@t-online.de>
2024-02-20 16:12:32 +0100

    downloaded some lyrics

--- src/utils.py
+++ src/utils.py
@@ -14,27 +14,19 @@ def get_main_tag(tags: List[str]):
     """ Find the main tag from a given list of tags and a list of genres. """
 
     # Get the main tag
-    tag_found = False
     for tag in reversed(tags):
         if tag in genres:
-            main_tag = tag
-            tag_found = True
-            break
+            return tag
 
     # If none of the genres matched, see if any of the secondary genre
     # strings contain a primary genre
-    if not tag_found:
-        for tag in reversed(tags):
-            for g in genres:
-                if g in tag:
-                    main_tag = g
-                    tag_found = True
+    for tag in reversed(tags):
+        for g in genres:
+            if g in tag:
+                return g
         
     # If there is still no match: set the genre to an empty string
-    if not tag_found:
-        main_tag = ""
-
-    return main_tag
+    return ""
 
 def shrink_genius_tag(tag: dict) -> str:
     return tag["name"].lower().replace("&", "-")
